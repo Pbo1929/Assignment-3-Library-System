92f140ca12764a84be9982eb7a5365b11351dc37
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-14 14:53:41 +0700

    Add project files.

---
+++ Assignment 3 Library System/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3_Library_System
+{
+    class Book
+    {
+        private string Title;
+        private string Author;
+        private int Pages;
+        private bool Borrowed;
+
+        public static int NumberOfBooks = 0;
+
+        public Book(string Title, string Author, int Pages)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Pages = Pages;
+            this.Borrowed = false;
+
+            NumberOfBooks = NumberOfBooks + 1;
+        }
+
+        public string GetTitle()
+        {
+            return Title;
+        }
+
+        public string GetAuthor()
+        {
+            return Author;
+        }
+
+        public int GetPages()
+        {
+            return Pages;
+        }
+
+        public bool isBorrowed()
+        {
+            return Borrowed;
+        }
+
+        public void BorrowBook()
+        {
+            if (Borrowed == false)
+            {
+                Borrowed = true;
+            }
+            else
+            {
+                Console.WriteLine(Title + "is not available.");
+            }
+        }
+
+        public void ReturnBook()
+        {
+            if(Borrowed == true)
+            {
+                Borrowed = false;
+            }
+            else
+            {
+                Console.WriteLine(Title + "is available.");
+            }
+        }
+    }
+}
---
+++ Assignment 3 Library System/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment_3_Library_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book harryPotter = new Book("Harry Potter", "JK Rowling", 399);
+            Console.WriteLine(Book.NumberOfBooks);
+            Console.WriteLine(harryPotter.GetTitle());
+            Console.WriteLine(harryPotter.GetAuthor());
+            Console.WriteLine(harryPotter.GetPages());
+            Console.WriteLine(harryPotter.isBorrowed());
+
+            harryPotter.BorrowBook();
+            Console.WriteLine(harryPotter.isBorrowed());
+            harryPotter.BorrowBook();
+
+            harryPotter.ReturnBook();
+            harryPotter.ReturnBook();
+
+        }
+    }
+}
